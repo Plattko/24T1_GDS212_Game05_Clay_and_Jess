360e7f2f3843d38cfe457f5f83b43fb6f733b2b8
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-03-11 21:28:29 +1100

    Set Up Flower, Tree and Place Subject Images
    - Created the flower, tree, and place scriptable objects using the pixelated sprites.
    - Added the scriptable objects to their respective lists in the ImagePicker.
    - Created a Strings script that is intended to store the image info strings, but is currently unused.

---
+++ Assets/Scripts/Strings.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FindingBeauty
+{
+    public class Strings : MonoBehaviour
+    {
+        public static string jasmineInfo = "Jasminum officinale - The national flower of Pakistan. The plant is highly valued in gardens for its intense pleasant fragrance.";
+        public static string lavenderInfo = "Lavandula - A flowering plant, known for its distinct violet hue. Often cultivated in temperate climates, lavender is commonly used in herbal medicine, cosmetics and essential oils.";
+        public static string lilyInfo = "Lilium - Lilies are a group flowering species. Most lily species are native to the Northern Hemisphere, thriving in temperate climates and extending into subtropical regions.";
+        public static string sunflowerInfo = "Helianthus annuus - First domesticated in the Americas, Sunflowers are commonly grown as a crop to collect its edible seeds, or create cooking oil.";
+        public static string tulipInfo = "Tulipa - Tulips are typically brightly coloured, large flowers grown in temperate climates. The name \"tulip\" is believed to originate from a Persian word meaning \"turban,\" reflecting its perceived resemblance to this headwear by its discoverers.";
+    }
+}
