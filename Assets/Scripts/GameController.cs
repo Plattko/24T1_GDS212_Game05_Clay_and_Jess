0a8a62ae6157530d2097a34008adbc0ee31cb568
jess-rae-miller <136469171+jess-rae-miller@users.noreply.github.com>
2024-03-14 21:20:17 +1100

    Refining text filter again
    After a merge conflict, I am repeating the same steps I did before when refining it originally.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System;
+using System.Linq;
 
 namespace FindingBeauty
 {
@@ -12,6 +14,7 @@ namespace FindingBeauty
         private Onboarding onboarding;
 
         [SerializeField] private TMP_InputField writingInputField;
+        [SerializeField] private TMP_Text popupMessage;
 
         private int progressionIndex = 1;
 
@@ -19,10 +22,31 @@ namespace FindingBeauty
         {
             imageDisplay = GetComponent<ImageDisplay>();
             onboarding = GetComponent<Onboarding>();
+            popupMessage.gameObject.SetActive(false);
         }
 
         public void SubmitButton()
         {
+            string inputText = writingInputField.text.Trim();
+            int wordCount = writingInputField.text.Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+            if (wordCount < 3 || inputText.Length < 10)
+            {
+                popupMessage.text = "Please write at least 3 words!";
+                popupMessage.gameObject.SetActive(true);
+                return;
+            }
+            else if (!IsInputValid(inputText))
+            {
+                popupMessage.text = "Please donn't keyboard smash :(";
+                popupMessage.gameObject.SetActive(true);
+                return;
+            }
+            else
+            {
+                popupMessage.gameObject.SetActive(false);
+            }
+
             Debug.Log("Progression index: " + progressionIndex);
 
             if (progressionIndex < 23)
@@ -39,6 +63,12 @@ namespace FindingBeauty
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
+        }
+
+        bool IsInputValid(string input)
+        {
+            var distinctCharCount = input.Distinct().Count();
+            return distinctCharCount > 5; // Ensure there's a variety of characters
         }
 
         public void PlayButton()
